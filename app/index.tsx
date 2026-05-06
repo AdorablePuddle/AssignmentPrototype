1cc7a21d367522c617434dbcac1cdfbc419554cb
AdorablePuddle <45329320+AdorablePuddle@users.noreply.github.com>
2026-05-06 22:43:07 +1000

    Basis

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,31 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, View } from "react-native";
 
 export default function Index() {
+	const [showMode, setShowMode] = useState(0); // 0 = Both, 1 = Steps, 2 = Time
+
 	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: "center",
-				alignItems: "center",
-			}}
-		>
-			<Text>Edit app/index.tsx to edit this screen.</Text>
+		<View style = {style.mainContainer}>
+			<View style = {style.buttonContainer}>
+				<Button title = "Show All" onPress = {() => {setShowMode(0)}}/>
+				<Button title = "Show Steps" onPress = {() => {setShowMode(1)}}/>
+				<Button title = "Show Time" onPress = {() => {setShowMode(2)}}/>
+			</View>
 		</View>
 	);
 }
+
+const style = StyleSheet.create({
+	mainContainer : {
+		flex: 1,
+		justifyContent : "center",
+		alignItems : "center",
+	},
+	buttonContainer : {
+		flex: 1,
+		flexDirection : "row",
+		justifyContent : "center",
+		alignItems : "center",
+		gap : 10,
+	}
+})
